9a5e341f28bd951d68c00e6a2ecccb053c10cda0
Shubham <shubhambhutada7810@gmail.com>
2023-12-10 00:34:27 +0530

    final commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,21 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [data, setData] = useState("");
   return (
     <div>
         {/* Do not remove the main div */}
+        <label htmlFor="greet">Enter your name:</label>
+        <br/>
+        <input
+        type="text"
+        id="greet"
+        name="greet"
+        value={data}
+        onChange={(e)=>setData(e.target.value)}/>
+        {(data==="")?<p></p>:<p>Hello {data}!</p>}
     </div>
   )
 }
